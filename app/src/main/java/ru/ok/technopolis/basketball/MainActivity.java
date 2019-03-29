758a8654113ec44b6c9440ac6d9d950b3c744977
Egor.Shakhmin <lapter57@gmail.com>
2019-03-29 19:15:40 +0300

    homework was resolved

--- app/src/main/java/ru/ok/technopolis/basketball/MainActivity.java
+++ app/src/main/java/ru/ok/technopolis/basketball/MainActivity.java
@@ -1,13 +1,225 @@
 package ru.ok.technopolis.basketball;
 
+import android.annotation.SuppressLint;
+import android.graphics.Point;
+import android.os.CountDownTimer;
+import android.support.animation.DynamicAnimation;
+import android.support.animation.FlingAnimation;
+import android.support.animation.SpringAnimation;
+import android.support.animation.SpringForce;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import ru.ok.technopolis.basketball.views.TallyCounter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long START_TIME_IN_MILLIS = 60_000;
+
+    private TextView timerView;
+    private Button startGameBtn;
+    private ImageView ballView;
+    private TallyCounter scoreView;
+    private TextView bestScoreView;
+
+    private CountDownTimer timer;
+    private boolean timerIsRunning;
+    private long timeLeftInMillis = START_TIME_IN_MILLIS;
+
+    private float[] target = {477, 515, 67, 85};
+    private boolean wasHit;
+    private int bestScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        convertDpToPixels(target);
+
+        timerView = findViewById(R.id.activity_main__timer);
+        startGameBtn = findViewById(R.id.activity_main__start_game);
+        ballView = findViewById(R.id.activity_main__ball);
+        scoreView = findViewById(R.id.activity_main__score_counter);
+        bestScoreView = findViewById(R.id.activity_main__best_score);
+
+        setBallAnimationThrowing();
+        startGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FlingAnimation rotateBall = new FlingAnimation(ballView, DynamicAnimation.ROTATION)
+                        .setFriction(1.1f)
+                        .setStartVelocity(10_000f);
+                rotateBall.start();
+                startGameBtn.setVisibility(View.GONE);
+                timerView.setVisibility(View.VISIBLE);
+                scoreView.reset();
+                startTimer();
+            }
+        });
+        bestScoreView.setText(getString(R.string.best, 0));
+
+    }
+
+    private void startTimer() {
+        timer = new CountDownTimer(START_TIME_IN_MILLIS, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateTimerView();
+            }
+
+            @Override
+            public void onFinish() {
+                bestScore = Math.max(bestScore, scoreView.getCount());
+                if (bestScore == scoreView.getCount()) {
+                    bestScoreView.setText(getString(R.string.best, bestScore));
+                }
+                timerIsRunning = false;
+                timerView.setVisibility(View.GONE);
+                startGameBtn.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+        timerIsRunning = true;
+    }
+
+    private void updateTimerView() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        timerView.setText(timeLeftFormatted);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setBallAnimationThrowing() {
+        final FlingAnimation flingAnimationX = new FlingAnimation(ballView, DynamicAnimation.X)
+                .setFriction(2f);
+        final FlingAnimation flingAnimationY = new FlingAnimation(ballView, DynamicAnimation.Y)
+                .setFriction(5f);
+
+        flingAnimationX.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation,
+                                       boolean canceled, float value,
+                                       float velocity)
+            {
+                float ballCenterPosX = ballView.getX() + ballView.getWidth() / 2;
+                float ballCenterPosY = ballView.getY() + ballView.getHeight() / 2;
+                if (estimateThrow(ballCenterPosX, ballCenterPosY)) {
+                    if (wasHit) {
+                        scoreView.setCount(scoreView.getCount() * 2);
+                    } else {
+                        scoreView.increment();
+                        wasHit = true;
+                    }
+                    animateHit();
+                } else {
+                    wasHit = false;
+                    rollbackOfThrowing();
+                }
+
+            }
+        });
+
+        final GestureDetector gestureDetector = new GestureDetector(this,
+                new GestureDetector.SimpleOnGestureListener() {
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+                flingAnimationX.setStartVelocity(velocityX);
+                flingAnimationY.setStartVelocity(velocityY);
+
+                flingAnimationX.start();
+                flingAnimationY.start();
+
+                return true;
+            }
+        });
+
+        ballView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (timerIsRunning) {
+                    gestureDetector.onTouchEvent(event);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Display display = getWindowManager().getDefaultDisplay();
+        final Point size = new Point();
+        display.getSize(size);
+        ballView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                flingAnimationX.setMinValue(0f).setMaxValue((float) (size.x - ballView.getWidth()));
+                flingAnimationY.setMinValue(0f).setMaxValue((float) (size.y - ballView.getHeight()));
+                ballView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+    }
+
+    private void animateHit(){
+        FlingAnimation dropInBasket = new FlingAnimation(ballView, DynamicAnimation.TRANSLATION_Y)
+                .setFriction(1.5f)
+                .setStartVelocity(5000f);
+        dropInBasket.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation dynamicAnimation,
+                                       boolean canceled, float value,
+                                       float velocity)
+            {
+                rollbackOfThrowing();
+            }
+        });
+        dropInBasket.start();
+    }
+
+    private void rollbackOfThrowing() {
+        SpringAnimation rollbackX = new SpringAnimation(ballView,DynamicAnimation.TRANSLATION_X);
+        SpringForce forceX = new SpringForce(0);
+        forceX.setDampingRatio(SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+        forceX.setStiffness(SpringForce.STIFFNESS_LOW);
+        rollbackX.setSpring(forceX);
+
+        SpringAnimation rollbackY = new SpringAnimation(ballView,DynamicAnimation.TRANSLATION_Y);
+        SpringForce forceY = new SpringForce(0);
+        forceY.setDampingRatio(SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+        forceY.setStiffness(SpringForce.STIFFNESS_LOW);
+        rollbackY.setSpring(forceX);
+
+        rollbackX.start();
+        rollbackY.start();
+    }
+
+    private boolean estimateThrow(float x, float y) {
+        return x >= target[0] && x <= target[1] && y >= target[2] && y <= target[3];
+    }
+
+    public void convertDpToPixels(float[] dp) {
+        for (int i = 0; i < dp.length; i++) {
+            float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp[i],
+                    this.getResources().getDisplayMetrics());
+            dp[i] = px;
+        }
     }
 }
---
+++ app/src/main/java/ru/ok/technopolis/basketball/views/ScoreCounter.java
@@ -0,0 +1,102 @@
+package ru.ok.technopolis.basketball.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.Locale;
+
+import ru.ok.technopolis.basketball.R;
+
+public class ScoreCounter extends View implements TallyCounter {
+
+    private static final int MAX_COUNT = 9999;
+    private static final String MAX_COUNT_STRING = String.valueOf(MAX_COUNT);
+
+    private int count;
+    private String displayedCount;
+
+    private TextPaint numberPaint;
+
+    public ScoreCounter(Context context) {
+        this(context, null);
+    }
+
+    public ScoreCounter(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        numberPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+
+        TypedArray typedArray = context
+                .obtainStyledAttributes(attrs, R.styleable.ScoreCounter, 0, 0);
+
+        int textColor = typedArray.getColor(R.styleable.ScoreCounter_android_textColor,
+                ContextCompat.getColor(context, android.R.color.black));
+        float textSize = Math.round(
+                typedArray.getDimensionPixelSize(R.styleable.ScoreCounter_android_textSize,
+                        Math.round(64f * getResources().getDisplayMetrics().scaledDensity)));
+        numberPaint.setColor(textColor);
+        numberPaint.setTextSize(textSize);
+        numberPaint.setTypeface(Typeface.SANS_SERIF);
+
+        typedArray.recycle();
+        setCount(0);
+    }
+
+    @Override
+    public void reset() {
+        setCount(0);
+    }
+
+    @Override
+    public void increment() {
+        setCount(count + 1);
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void setCount(int count) {
+        count = Math.min(count, MAX_COUNT);
+        this.count = count;
+        this.displayedCount = String.format(Locale.getDefault(), "%04d", count);
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Paint.FontMetrics fontMetrics = numberPaint.getFontMetrics();
+
+        float maxTextWidth = numberPaint.measureText(MAX_COUNT_STRING);
+        float maxTextHeight = fontMetrics.bottom - fontMetrics.top;
+
+        int desiredWidth = Math.round(maxTextWidth + getPaddingLeft() + getPaddingRight());
+        int desiredHeight = Math.round(maxTextHeight * 2f + getPaddingTop() + getPaddingBottom());
+
+        int measuredWidth = resolveSize(desiredWidth, widthMeasureSpec);
+        int measuredHeight = resolveSize(desiredHeight, heightMeasureSpec);
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        final int canvasWidth = canvas.getWidth();
+        final int canvasHeight = canvas.getHeight();
+        final float centerX = canvasWidth * 0.5f;
+
+        final float baselineY = Math.round(canvasHeight * 0.6f);
+        final float textWidth = numberPaint.measureText(displayedCount);
+        final float textX = Math.round(centerX - textWidth * 0.5f);
+        canvas.drawText(displayedCount, textX, baselineY, numberPaint);
+    }
+}
