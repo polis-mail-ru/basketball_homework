8ead8e49efa0e4cbc25d15bfd9fda72067bfdbe8
Nadya <spinakernadya7@gmail.com>
2019-05-04 18:37:58 +0300

    def

--- app/src/main/java/ru/ok/technopolis/basketball/MainActivity.java
+++ app/src/main/java/ru/ok/technopolis/basketball/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.ok.technopolis.basketball;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.animation.Animator;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         ball = findViewById(R.id.activity_main_ball);
         hoop = findViewById(R.id.activity_main_hoop);
 
-        final ConstraintLayout activityMain = findViewById(R.id.activity_main);
+        final ConstraintLayout idMain = findViewById(R.id.activity_main);
 
         final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
             @Override
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        activityMain.setOnTouchListener(new View.OnTouchListener() {
+        idMain.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 gestureDetector.onTouchEvent(event);
@@ -59,27 +60,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean AnimatorTrack(final float X, final float Y, final float xHoop, final float yHoop, final float targetSize) {
-        ValueAnimator aniView = ValueAnimator.ofFloat(0, 1);
+        ValueAnimator anyView = ValueAnimator.ofFloat(0, 1);
 
-        aniView.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        anyView.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 float x = ball.getX();
                 float y = ball.getY();
                 int rotation = 50 + (int) (Math.random() * 101);
 
-                float value = (Float) valueAnimator.getAnimatedValue();
+                float value = (float) valueAnimator.getAnimatedValue();
                 float angle = value * 3.14f;
 
                 ball.setRotation(rotation * value);
                 ball.setTranslationX((float) ((1 - Math.cos(angle)) * X));
                 ball.setTranslationY((float) (-Math.sin(angle) * Math.abs(Y)));
 
-                if ((Math.abs(xHoop - x) <= targetSize) && (Math.abs(yHoop - y) <= targetSize)) hit = true;
+                if ((Math.abs(xHoop - x) <= targetSize) && (Math.abs(yHoop - y) <= targetSize)) { hit = true;}
             }
         });
 
-        aniView.addListener(new AnimatorListenerAdapter() {
+        anyView.addListener(new AnimatorListenerAdapter() {
 
             @Override
             public void onAnimationStart(Animator animation) {
@@ -104,47 +105,44 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        aniView.setDuration(1200);
-        aniView.start();
+        anyView.setDuration(1200);
+        anyView.start();
 
         return true;
     }
 
     public void SetStars(int hits) {
-        ImageView star;
+        Drawable redStar = getResources().getDrawable(R.drawable.ic_grade_red_24dp);
+        Drawable blackStar = getResources().getDrawable(R.drawable.ic_grade_black_24dp);
+        ImageView star1=findViewById(R.id.image_Star1);
+        ImageView star2=findViewById(R.id.image_Star2);
+        ImageView star3=findViewById(R.id.image_Star3);
+        ImageView star4=findViewById(R.id.image_Star4);
+        ImageView star5=findViewById(R.id.image_Star5);
+
         switch (hits) {
             case 1:
-                star = findViewById(R.id.imageStar1);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_red_24dp));
+                star1.setImageDrawable(redStar);
                 break;
             case 2:
-                star = findViewById(R.id.imageStar2);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_red_24dp));
+                star2.setImageDrawable(redStar);
                 break;
             case 3:
-                star = findViewById(R.id.imageStar3);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_red_24dp));
+                star3.setImageDrawable(redStar);
                 break;
             case 4:
-                star = findViewById(R.id.imageStar4);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_red_24dp));
+                star4.setImageDrawable(redStar);
                 break;
             case 5:
-                star = findViewById(R.id.imageStar5);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_red_24dp));
+                star5.setImageDrawable(redStar);
                 hits = 0;
                 break;
             default:
-                star = findViewById(R.id.imageStar1);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_black_24dp));
-                star = findViewById(R.id.imageStar2);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_black_24dp));
-                star = findViewById(R.id.imageStar3);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_black_24dp));
-                star = findViewById(R.id.imageStar4);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_black_24dp));
-                star = findViewById(R.id.imageStar5);
-                star.setImageDrawable(getResources().getDrawable(R.drawable.ic_grade_black_24dp));
+                star1.setImageDrawable(blackStar);
+                star2.setImageDrawable(blackStar);
+                star3.setImageDrawable(blackStar);
+                star4.setImageDrawable(blackStar);
+                star5.setImageDrawable(blackStar);
                 hits = 0;
                 break;
         }
