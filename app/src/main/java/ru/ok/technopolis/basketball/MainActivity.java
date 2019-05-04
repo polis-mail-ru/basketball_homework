a8b2231d1b628c96c12eeb37994a8b24cbeae401
Igorlo <9440533@gmail.com>
2019-05-04 21:26:11 +0300

    CustomView, ball throw - work in progress

--- app/src/main/java/ru/ok/technopolis/basketball/MainActivity.java
+++ app/src/main/java/ru/ok/technopolis/basketball/MainActivity.java
@@ -1,13 +1,87 @@
 package ru.ok.technopolis.basketball;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private StarScoreView scoreView;
+    private ImageView hoop;
+    private ImageView ball;
+    private float dX = 0;
+    private float dY = 0;
+    private float prevX = 0;
+    private float prevY = 0;
+
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreView = findViewById(R.id.activity_main__scores);
+        ball = findViewById(R.id.ball);
+        hoop = findViewById(R.id.hoop);
+
+        ball.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View ball, MotionEvent event) {
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+                        dX = ball.getX() - event.getRawX();
+                        dY = ball.getY() - event.getRawY();
+                        prevX = event.getRawX();
+                        prevY = event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        ball.animate()
+                                .x(event.getRawX() + dX)
+                                .y(event.getRawY() + dY)
+                                .setDuration(0)
+                                .start();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        float destinationX = ball.getX() + (event.getRawX() - prevX) * 4.5f;
+                        float destinationY = ball.getY() + (event.getRawY() - prevY) * 3f;
+                        ball.setY(prevY + dY);
+                        ball.setX(prevX + dX);
+                        ball.animate()
+                                .x(destinationX)
+                                .y(destinationY)
+                                .setDuration(500).
+                                withEndAction(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        reload();
+                                    }
+                                }).start();
+                        if (destinationX > hoop.getX() + hoop.getWidth() / 4
+                                && destinationX < hoop.getX() + hoop.getWidth() / 2
+                                && destinationY < hoop.getY() + hoop.getHeight()
+                                && destinationY > hoop.getY() + hoop.getHeight() / 3) {
+                            scoreView.incrementScore();
+                        } else {
+                            scoreView.decrementScore();
+                        }
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void reload() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        ball.setX(120 * metrics.density);
+        ball.setY((metrics.heightPixels - 80) / metrics.density);
     }
 }
---
+++ app/src/main/java/ru/ok/technopolis/basketball/StarScoreView.java
@@ -0,0 +1,72 @@
+package ru.ok.technopolis.basketball;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class StarScoreView extends View {
+
+    byte score = 0;
+    byte starCount = 0;
+    boolean isCombo = false;
+
+    int starSide = 0;
+    int offsetStep = 0;
+    Drawable defaultStar = getResources().getDrawable(R.drawable.star_default);
+    Drawable comboStar = getResources().getDrawable(R.drawable.star_combo);
+
+    public StarScoreView(Context context) {
+        super(context, null);
+    }
+
+    public StarScoreView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void incrementScore() {
+        System.out.println("kek");
+        if (score == 10)
+            return;
+        score++;
+        starCount++;
+        if (score == 6) {
+            starCount = 1;
+            isCombo = true;
+        }
+        invalidate();
+    }
+
+    public void decrementScore() {
+        System.out.println("kek");
+        if (score == 0)
+            return;
+        score--;
+        starCount--;
+        if (score == 5) {
+            starCount = 5;
+            isCombo = false;
+        }
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable star = isCombo ? comboStar : defaultStar;
+        for (int i = 0; i < starCount; i++) {
+            star.setBounds(i * offsetStep, 0, i * offsetStep + starSide, starSide);
+            star.draw(canvas);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        starSide = (int) Math.min(h, w / 5f);
+        offsetStep = w / 5;
+    }
+
+}
